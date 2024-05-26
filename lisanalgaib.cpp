0c0336d3e10d4e5897efb7c13aa1f7845ca08d5e
keyloor <150095023+keyloor@users.noreply.github.com>
2024-05-26 11:31:55 -0600

    Add files via upload

---
+++ lisanalgaib.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    
+    std::cout << "LISAN AL GAIB SAYS: \n";
+
+    srand(time(NULL));
+    int num = std::rand() % 10;
+
+    switch (num) {
+        case 0:
+            std::cout << "The spice must flow.\nYes, move forward.\n";
+            break;
+        case 1:
+            std::cout << "Fear is the mind-killer.\nOvercome your fear.\n";
+            break;
+        case 2:
+            std::cout << "He who controls the spice controls the universe.\nYou have the power.\n";
+            break;
+        case 3:
+            std::cout << "Muad'Dib knows.\nTrust your instincts.\n";
+            break;
+        case 4:
+            std::cout << "The desert teaches harsh lessons.\nBe patient.\n";
+            break;
+        case 5:
+            std::cout << "The Sleeper must awaken.\nNow is the time to act.\n";
+            break;
+        case 6:
+            std::cout << "The slow blade penetrates the shield.\nPersistence will pay off.\n";
+            break;
+        case 7:
+            std::cout << "I will bend like a reed in the wind.\nAdapt to the situation.\n";
+            break;
+        case 8:
+            std::cout << "A storm is coming.\nPrepare for challenges.\n";
+            break;
+        case 9:
+            std::cout << "Desert power is supreme.\nEmbrace your strength.\n";
+            break;
+    }
+
+    std::cout << "                                .-.._    ___\n";
+    std::cout << "                            ,-'   |:\\ /'--.`.\n";
+    std::cout << "                           /'    ,|:|/::::| '\\\n";
+    std::cout << "                         ,'  ,.-:::::::::::.  \\\n";
+    std::cout << "                       _/' /::::::::::::::::`  \\\n";
+    std::cout << "                      /'  ':::::::_,--.::::::`.'\\\n";
+    std::cout << "                     /  ,'::::.,-'     `\\::::::||\n";
+    std::cout << "                    |  /:::_/'           `\\::::||\n";
+    std::cout << "                    |  \\::-                \\:::||\n";
+    std::cout << "                    /_                      \\_/ |\n";
+    std::cout << "                   |  '-._                      /\n";
+    std::cout << "                   |'.    '`-..._              |\n";
+    std::cout << "                   ..  '--_             ''----.|\n";
+    std::cout << "                   | `._   '-----  '''----__,..\\ \n";
+    std::cout << "                   |.._ '-.._________..--'   ,'|_\n";
+    std::cout << "                   |   `--..__            _,' /| ''--.\n";
+    std::cout << " ''''''---..__     |          '`-------'''   /-\\ .-.._\\_\n";
+    std::cout << "              '-\\__ \\-----....__        _..'   ,.-. `. .`\\\n";
+    std::cout << "                   '`.          \"\"\"--\"\"\"    _/' _. \\ \\  | \\\n";
+    std::cout << "                      \\--..___       ___..--   / | ' |  |  \\   ___...---\n";
+    std::cout << "             _,........\\      \"\"\"--\"\"        ./  |/-..__=..|,-'\n";
+    std::cout << "   ______, ''           `            __,...-''  ,'     -.. '\n";
+    std::cout << ",-'                      `.-...,-''''          - |        '`----------\n";
+    std::cout << "                          \\              _.,-'' .'\n";
+    std::cout << "                           \\ ..______.,-'      ,'\n";
+    std::cout << "                            '-.             ,/'\n";
+    std::cout << "                               ''---'......-'\n";
+    std::cout << "                                                    /`-._\n";
+    std::cout << "                                                    |::::\"\"--.._\n";
+    std::cout << "                                /(     )\\           /::::::;;;;;\\____\n";
+    std::cout << "                                \\\\  _  //          /;;::::::;;;;;;;;;\\\n";
+    std::cout << "                    _           / \\(_)/ \\         /;;;;:::::::;;;;;;;;;\\\n";
+    std::cout << "                   /;|          |       <        |;;;;;;;:::::::;;;;;;:::;\\\n";
+    std::cout << "              ,-._/;;\\           \\_    /        /;;;;;;;;;:::::::::::;;;;;;|\n";
+    std::cout << "         _.-;;::::;;;;\\           /    |       /;;;;;:::::::::;;;:::::;;;;;;\\\n";
+    std::cout << "        /;;::;;;;;;;;;;\\          |    /     _/;;;;::::::::::;;;;;::::;;;;;;;;;\\\n";
+    std::cout << "    _.-;::::::;;;;;;;;;;\\         \\__  \\    /;;;;;:::::::::;;;;;::::;;;;;;;;;;\\\n";
+    std::cout << "_.-;;;::::::::::;;;;;;;;;\\           `-'   /;;;;:::::::::;;;;;;;;;::;;;;;;;;;;;\\\n";
+    std::cout << ";;;;;:::::::::::::;;;;;;;;|               /;;;;;:::::;;;;;;;;;;;:::::;;;;;;;;;;;;\\\n";
+
+    return 0;
+}
